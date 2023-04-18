5bead4535c7b2102c601566a5f9f110317844292
Soumya Pallebothula <51355605+Soumyapallebothula@users.noreply.github.com>
2023-04-18 17:04:49 +0530

    2

--- Average Selling Price.sql
+++ Average Selling Price.sql
@@ -1,40 +1,4 @@
 
-Theme :
-Write an SQL query to find the average selling price for each product. average_price should be rounded to 2 decimal places.
-
-Input: 
-Prices table :
-+------------+------------+------------+--------+
-| product_id | start_date | end_date   | price  |
-+------------+------------+------------+--------+
-| 1          | 2019-02-17 | 2019-02-28 | 5      |
-| 1          | 2019-03-01 | 2019-03-22 | 20     |
-| 2          | 2019-02-01 | 2019-02-20 | 15     |
-| 2          | 2019-02-21 | 2019-03-31 | 30     |
-+------------+------------+------------+--------+
-UnitsSold table:
-+------------+---------------+-------+
-| product_id | purchase_date | units |
-+------------+---------------+-------+
-| 1          | 2019-02-25    | 100   |
-| 1          | 2019-03-01    | 15    |
-| 2          | 2019-02-10    | 200   |
-| 2          | 2019-03-22    | 30    |
-+------------+---------------+-------+
-Output: 
-+------------+---------------+
-| product_id | average_price |
-+------------+---------------+
-| 1          | 6.96          |
-| 2          | 16.96         |
-+------------+---------------+
-Explanation: 
-Average selling price = Total Price of Product / Number of products sold.
-Average selling price for product 1 = ((100 * 5) + (15 * 20)) / 115 = 6.96
-Average selling price for product 2 = ((200 * 15) + (30 * 30)) / 230 = 16.96
-
-Solution :
-
 SELECT a.product_id,ROUND(SUM(b.units*a.price)/SUM(b.units),2) as average_price
 FROM Prices as a
 JOIN UnitsSold as b
