270cce7f2d78f44258e7a5093c3714a68699f658
Soumya Pallebothula <51355605+Soumyapallebothula@users.noreply.github.com>
2023-04-21 14:01:44 +0530

    Actors and Directors

--- 1.sql
+++ 1.sql
@@ -2,7 +2,6 @@ QNS LINK : https://leetcode.com/problems/actors-and-directors-who-cooperated-at-
 
 Theme:
 Write a SQL query for a report that provides the pairs (actor_id, director_id) where the actor has cooperated with the director at least three times.
-Return the result table in any order.
 
 Input: 
 ActorDirector table:
